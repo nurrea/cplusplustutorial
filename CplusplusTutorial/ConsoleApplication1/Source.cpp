be596322a54da19ef6157a52ea17fdf9a39f1165
nurrea <nurrea@gmail.com>
2015-01-15 23:30:18 -0800

    init

---
+++ CplusplusTutorial/ConsoleApplication1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const double pi = 3.14159;
+const char newline = '\n';
+
+int main()
+{
+	string mystring;
+	mystring = "This is a string";
+	cout << mystring << endl;
+	return 0;
+}
