227ed63762de920b2f741504b977c71e4b0a5a79
Nick Urrea <nurrea@gmail.com>
2015-01-25 11:47:46 -0800

    tina helped me with c++

--- CplusplusTutorial/ConsoleApplication1/Source.cpp
+++ CplusplusTutorial/ConsoleApplication1/Source.cpp
@@ -7,31 +7,6 @@ using namespace std;
 const double pi = 3.14159;
 const char newline = '\n';
 
-int main()
-{
-	std::vector<int> vDummy;
-	for (int i = 0; i < 10; i++)
-	{												
-		vDummy.push_back(i * 10);
-	}
-
-	std::vector<int> list;
-	for (auto it = std::begin(vDummy); it != std::end(vDummy); ++it)
-	{
-		list.push_back(*it);
-	}
-
-	std::cout << "bar contains";
-	for (auto it = std::begin(list); it != std::end(list); ++it)
-	{
-		std::cout << ' ' << *it;
-	}
-
-	string test;
-	cin >> test;
-	return 0;
-}
-
 enum GamePlayerNumber
 {
 	PlayerOne,
@@ -60,30 +35,100 @@ class Player
 public:
 	Player();
 	Player(string, GamePlayerNumber);
+	void something();
+	~Player();
 };
 
-Player::Player()
+Player::~Player()
 {
 
 }
 
-Player::Player(string p_name, GamePlayerNumber playerNumber)
+Player::Player()
 {
-	name = p_name;
-	m_playerNumber = playerNumber;
+	
+}
+
+Player::Player(string i_name, GamePlayerNumber i_playerNumber)
+{
+	name = i_name;
+	m_playerNumber = i_playerNumber;
+}
+void Player::something()
+{
+
 }
 
 class GameBoard
 {
-	Player player1;
-	Player player2;
+	Player *m_player1;
+	Player *m_player2;
 
 	public:
 		GameBoard();
+		~GameBoard();
+		GameBoard(Player *p1, Player *p2);
+		int init();
+		int deinit();
+		
 };
+
+int GameBoard::init()
+{
+	return 0;
+}
+
+int GameBoard::deinit()
+{
+	delete m_player1;
+	m_player1 = NULL;
+	return 0;
+}
+
+GameBoard::~GameBoard()
+{
+}
 
 GameBoard::GameBoard()
 {
-	//player1 = new Player("Nick", PlayerOne);
-	//player2 = new Player("Tina", PlayerTwo);
+	//m_player1 = NULL;
+	m_player2 = NULL;
+}
+
+GameBoard::GameBoard(Player *player1, Player *player2)
+{
+	m_player1 = player1;
+	m_player2 = player2;
+}
+
+int main()
+{
+	//Player *p1 = new Player("Nick", PlayerOne);
+	//Player *p2 = new Player("Tina", PlayerTwo);
+	//GameBoard *gb = new GameBoard(p1, p2);
+	GameBoard *gb = new GameBoard();
+	gb->init();
+	gb->deinit();
+
+	std::vector<int> vDummy;
+	for (int i = 0; i < 10; i++)
+	{
+		vDummy.push_back(i * 10);
+	}
+
+	std::vector<int> list;
+	for (auto it = std::begin(vDummy); it != std::end(vDummy); ++it)
+	{
+		list.push_back(*it);
+	}
+
+	std::cout << "bar contains";
+	for (auto it = std::begin(list); it != std::end(list); ++it)
+	{
+		std::cout << ' ' << *it;
+	}
+
+	string test;
+	cin >> test;
+	return 0;
 }
