30478643d1cd4c967432cdb993ae42f7b555100b
Nick Urrea <nurrea@gmail.com>
2015-01-24 22:09:13 -0800

    adding some classes

--- CplusplusTutorial/ConsoleApplication1/Source.cpp
+++ CplusplusTutorial/ConsoleApplication1/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,8 +9,81 @@ const char newline = '\n';
 
 int main()
 {
-	string mystring;
-	mystring = "This is a string";
-	cout << mystring << endl;
+	std::vector<int> vDummy;
+	for (int i = 0; i < 10; i++)
+	{												
+		vDummy.push_back(i * 10);
+	}
+
+	std::vector<int> list;
+	for (auto it = std::begin(vDummy); it != std::end(vDummy); ++it)
+	{
+		list.push_back(*it);
+	}
+
+	std::cout << "bar contains";
+	for (auto it = std::begin(list); it != std::end(list); ++it)
+	{
+		std::cout << ' ' << *it;
+	}
+
+	string test;
+	cin >> test;
 	return 0;
+}
+
+enum GamePlayerNumber
+{
+	PlayerOne,
+	PlayerTwo
+};
+
+enum GamePieceType
+{
+	Normal,
+	King
+};
+
+class GamePiece
+{
+	int x;
+	int y;
+	GamePieceType type;
+};
+
+class Player
+{
+	string name;
+	GamePlayerNumber m_playerNumber;
+	std::vector<GamePiece> m_pieces;
+
+public:
+	Player();
+	Player(string, GamePlayerNumber);
+};
+
+Player::Player()
+{
+
+}
+
+Player::Player(string p_name, GamePlayerNumber playerNumber)
+{
+	name = p_name;
+	m_playerNumber = playerNumber;
+}
+
+class GameBoard
+{
+	Player player1;
+	Player player2;
+
+	public:
+		GameBoard();
+};
+
+GameBoard::GameBoard()
+{
+	//player1 = new Player("Nick", PlayerOne);
+	//player2 = new Player("Tina", PlayerTwo);
 }
